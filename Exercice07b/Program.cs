3e3c2a04d09ee10a44aae3dc17d1c5976251f84b
ppoulincsf <122027944+ppoulincsf@users.noreply.github.com>
2023-10-10 09:00:42 -0400

    Add files via upload

---
+++ Exercice07b/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercice07
+{
+  public class Program
+  {
+    public static void Main ( string[] args )
+    {      
+      PrimeNumber ();
+      ReverseNumber ();
+	  FindNumber ();
+    }
+
+    
+    public static void PrimeNumber ()
+    {
+      
+    }
+
+    public static void ReverseNumber ()
+    {
+      
+    }
+	
+	public static void FindNumber ()
+    {
+      
+    }
+  }
+}
+
